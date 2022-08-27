f3fefe6ff670e31002500e658cd5c844393d9bbb
Jey <andre.marqquez@gmail.com>
2022-08-27 11:46:34 -0300

    feat [middleware] : middleware para validar info
    Adiciona middleware para validar informações no corpo da requisição para cadastro de usuario

---
+++ src/middlewares/validateInfoRegistration.js
@@ -0,0 +1,41 @@
+const joi = require('joi');
+const { ErrorBadRequest } = require('../errors/ErrorBadRequest');
+const { User } = require('../database/models');
+const { ErrorConflict } = require('../errors/ErrorConflict');
+require('express-async-errors');
+
+const schemaRegistration = joi.object({
+  displayName: joi.string().min(8).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+  image: joi.required(),
+});
+
+const handleError = (error) => {
+  const messageError = error.details[0].message;
+  const typeError = error.details[0].type;
+  switch (typeError) {
+    case 'any.required':
+      throw new ErrorBadRequest(messageError);
+    case 'string.min':
+      throw new ErrorBadRequest(messageError);
+    default:
+      throw new ErrorBadRequest(messageError);
+  }
+};
+
+const checkUserExists = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  if (user) throw new ErrorConflict('User already registered');
+  return false;
+};
+
+const validateInfoRegistration = async (req, _res, next) => {
+  await checkUserExists(req.body.email);
+  const validate = schemaRegistration.validate(req.body);
+  const { error } = validate;
+  if (error) handleError(error);
+  next();
+};
+
+module.exports = { validateInfoRegistration };
--- src/routes/routesUser.js
+++ src/routes/routesUser.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const { validateInfoRegistration } = require('../middlewares/validateInfoRegistration');
 
 const router = express.Router();
 
-router.post('/', userController.addUser);
+router.post('/', validateInfoRegistration, userController.addUser);
 
 module.exports = router;
