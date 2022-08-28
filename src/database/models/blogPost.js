0c52f2ea2f99e3012c0de9505283fe0aefbb4342
Jey <andre.marqquez@gmail.com>
2022-08-28 11:24:51 -0300

    feat [models] : model para tabela BlogPost
    Cria model para tabela BlogPost e associa UserID a tabela User

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      tableName: 'BlogPost',
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'id',
+      as: 'User'
+    });
+    }
+  return BlogPost;
+};
