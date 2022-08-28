e88116d8a457e4e409d953b8c46e44c2238c0b95
Jey <andre.marqquez@gmail.com>
2022-08-28 01:50:54 -0300

    feat [error] : Error customizado para HTTP 401
    Adiciona erro customizado para tratamento de erros com o HTTP 401

---
+++ src/errors/ErrorUnauthorized.js
@@ -0,0 +1,11 @@
+class ErrorUnauthorized extends Error {
+  constructor(message) {
+    super(message);
+    this.message = message;
+    this.code = 401;
+  }
+}
+
+module.exports = {
+  ErrorUnauthorized,
+};
