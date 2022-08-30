e08594a1ade5616858df546927a46017eb8a0693
Jey <andre.marqquez@gmail.com>
2022-08-30 16:08:14 -0300

    feat [services]: services para blogpost

---
+++ src/services/blogPostService.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+const { BlogPost, PostCategory, User } = require('../database/models');
+const { ErrorInternalServer } = require('../errors/ErrorInternalServer');
+const config = require('../database/config/config');
+
+const sequelize = new Sequelize(config.development);
+
+const { ErrorNotFound } = require('../errors/ErrorNotFound');
+require('express-async-errors');
+
+const blogPostService = {
+  findUser: async (email) => {
+    const { dataValues: user } = await User.findOne({ where: { email } });
+    if (!user) throw new ErrorNotFound('User não encontrado');
+    return user;
+  },
+  // findPost: async ({ title, content, userId }) => {
+  //   const { dataValues } = await BlogPost.findOne({
+  //     where: {
+  //       title,
+  //       content,
+  //       userId,
+  //     },
+  //   });
+  //   if (!dataValues) throw new ErrorNotFound('Post não encontrado');
+  //   return dataValues;
+  // },
+  addInIntermediateTable: async (postId, categoryIds, transaction) => {
+    try {
+      await Promise.all(
+        categoryIds.map((categoryId) =>
+          PostCategory.create({ postId, categoryId }, { transaction })),
+      );
+      return true;
+    } catch (error) {
+      throw new ErrorInternalServer('Problema ao cadastrar postCategory');
+    }
+  },
+  addBlogPost: async ({ title, content, userId, categoryIds }) => {
+    try {
+      const result = await sequelize.transaction(async (transaction) => {
+        const post = await BlogPost.create(
+          {
+            title,
+            content,
+            userId,
+          },
+          { transaction },
+        );
+        await blogPostService.addInIntermediateTable(post.id, categoryIds, transaction);
+        return post;
+      });
+      return result;
+    } catch (error) {
+      throw new ErrorInternalServer('Problema ao cadastrar post');
+    }
+  },
+};
+
+module.exports = blogPostService;
