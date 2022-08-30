3d3a311f05ad929dfac33518da86dbfe8e308ad5
Jey <andre.marqquez@gmail.com>
2022-08-30 16:07:56 -0300

    feat [services] : categories services

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -3,6 +3,17 @@ require('express-async-errors');
 const { ErrorBadRequest } = require('../errors/ErrorBadRequest');
 
 const categoriesService = {
+  checkCategoryExists: async (categoryId) => {
+    const response = await Category.findAll({
+      where: {
+        id: categoryId,
+      },
+    });
+    if (response.length > 0) {
+      return true;
+    }
+    return false;
+  },
   addCategory: async (name) => {
     if (!name) throw new ErrorBadRequest('"name" is required');
     const { dataValues } = await Category.create({ name });
