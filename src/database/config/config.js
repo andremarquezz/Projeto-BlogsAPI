de09347f5d49fd5b907834c5b424c0b12fa08132
Jey <andre.marqquez@gmail.com>
2022-09-07 15:07:58 -0300

    refactor [DB] : altera exibição de log
    Altera para não exibir log no ambiente de teste

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -11,15 +11,16 @@ const suffix = {
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  database: `${process.env.MYSQL_DB_NAME || 'blogs-api'}${
+    suffix[environment] || suffix.test
+  }`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
-  logging: process.env.DEBUG !== 'false',
+  logging: process.env.NODE_ENV !== 'test',
 };
 
 module.exports = {
