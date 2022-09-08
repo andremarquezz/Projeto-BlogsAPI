d4d6f9290f394da650df40ff0251296ea099aafa
Jey <andre.marqquez@gmail.com>
2022-09-08 13:07:52 -0300

    feat [tests]: Adiciona testes na rota /login

---
+++ src/tests/integration/login.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const app = require('../../api');
+import resetDatabase from './assets/resetDatabase';
+
+describe('Testes no endpoint < /login >', () => {
+  
+  beforeAll(async () => {
+    resetDatabase();
+  });
+
+  describe('Testes em /login no metodo < POST >', () => {
+    it('Testa se recebe um token quando passado informações de login corretas', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'lewishamilton@gmail.com',
+        password: '123456',
+      });
+      expect(response.status).toBe(200);
+      expect(response.body.token).toBeDefined();
+    });
+    it('Testa se recebe messagem de erro caso falte informação no body', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'lewishamilton@gmail.com',
+      });
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({ message: 'Some required fields are missing' });
+    });
+    it('Testa se recebe messagem de erro caso as informações de login estejam erradas', async () => {
+      const response = await request(app).post('/login').send({
+        email: 'lewishamilton@gmail.com',
+        password: '12345',
+      });
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({ message: 'Invalid fields' });
+    });
+  });
+});
