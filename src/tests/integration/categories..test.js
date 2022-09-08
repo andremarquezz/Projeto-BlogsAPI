86d3863b1a28ebc1299c4dad91e0d14c3f7f6cf9
Jey <andre.marqquez@gmail.com>
2022-09-08 13:08:08 -0300

    chore [tests]: testes na rota /categories

---
+++ src/tests/integration/categories..test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../../api');
+const allCategories = require('../assets/allCategories.json');
+const jwt = require('jsonwebtoken');
+import resetDatabase from './assets/resetDatabase';
+
+describe('Testes no endpoint < /categories > ', () => {
+  const jwtSpy = jest.spyOn(jwt, 'verify');
+  jwtSpy.mockReturnValue({ email: 'lewishamilton@gmail.com' });
+  const token = 'simulaToken';
+
+  beforeAll(async () => {
+    resetDatabase();
+  });
+
+  describe('Testes em /categories no metodo < GET >', () => {
+    it('Testa se é possivel pegar todas as categorias', async () => {
+      const response = await request(app)
+        .get('/categories')
+        .set({ authorization: token });
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(allCategories);
+    });
+  });
+  describe('Testes em /categories no metodo < POST >', () => {
+    it('Testa se é possivel adicionar uma nova categoria', async () => {
+      const response = await request(app)
+        .post('/categories')
+        .set({ authorization: token })
+        .send({
+          name: 'Batattinhas',
+        });
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        id: 3,
+        name: 'Batattinhas',
+      });
+    });
+  });
+});
